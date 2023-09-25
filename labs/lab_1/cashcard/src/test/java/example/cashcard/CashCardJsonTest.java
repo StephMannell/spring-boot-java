c435c1ce5e0002ba7a3a101bbc216ef2aefe2790
Stephanie Mannell <stephaniemannell84@gmail.com>
2023-09-25 19:06:34 +0000

    added CashCardJsonTest.java

---
+++ labs/lab_1/cashcard/src/test/java/example/cashcard/CashCardJsonTest.java
@@ -0,0 +1,11 @@
+package example.cashcard;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+public class CashCardJsonTest{
+    @Test
+    public static void myFirstTest() {
+        assertThat(1).isEqualto(42);
+    }
+
+}
+
