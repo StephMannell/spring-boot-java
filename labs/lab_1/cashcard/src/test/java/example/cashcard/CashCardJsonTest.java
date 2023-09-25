c583ddcaae013df9ee434f3f7b107855cb072977
Stephanie Mannell <stephaniemannell84@gmail.com>
2023-09-25 19:36:29 +0000

    adding lab 1

--- labs/lab_1/cashcard/src/test/java/example/cashcard/CashCardJsonTest.java
+++ labs/lab_1/cashcard/src/test/java/example/cashcard/CashCardJsonTest.java
@@ -4,7 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CashCardJsonTest{
     @Test
     public static void myFirstTest() {
-        assertThat(1).isEqualto(42);
+        assertThat(1).isEqualTo(42);
     }
 
 }
